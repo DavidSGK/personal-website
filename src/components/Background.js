84069cb94c6c56c804d2ae6ccd0d039b5cab5dc0
David Kang <hpdk97@gmail.com>
2017-02-03 23:42:00 -0500

    Removed pt background on mobile

--- src/components/Background.js
+++ src/components/Background.js
@@ -10,59 +10,62 @@ class Background extends React.Component {
 
     }
   }
-  /*componentDidMount(){
+  componentDidMount(){
+    if(window.matchMedia("(min-width: 769px)").matches){
 
-    var space = new Pt.CanvasSpace("pt").setup({bgcolor: Colors.blueGrey});
+      var space = new Pt.CanvasSpace("pt").setup({bgcolor: Colors.blueGrey});
 
-    var form = new Pt.Form(space);
-    form.stroke(false);
+      var form = new Pt.Form(space);
+      form.stroke(false);
 
-    var particles = [];
+      var particles = [];
 
-    class Particle extends Pt.Triangle {
-      constructor(x, y, length){
-        super(x, y);
-        this.to([x-length/2, y-length/2*Math.sqrt(3)], [x+length/2, y-length/2*Math.sqrt(3)]);
-        //this.color = colors["a"+Math.ceil(Math.random()*4)];
-        this.color = "#DDDDDD";
-        this.direction = new Pt.Vector((Math.random()-0.5) * 6, (Math.random()-0.5) * 6);
-        this.maxAge = Math.random() * 1000;
-        this.age = this.maxAge
-      }
-      animate(time, fps, context){
-        form.fill(this.color);
-        form.triangle(this);
-        this.rotate2D(0.4 / fps, this.orthocenter());
-        this.moveBy(this.direction);
-        this.age -= 0.5;
-        this.color = Pt.Util.toRGBColor("#DDDDDD", true, this.age/this.maxAge);
-        if(this.age < 0 || this.x < -10 || this.x > space.size.x + 10|| this.y < -10 || this.y > space.size.y + 10){
-          space.remove(this);
+      class Particle extends Pt.Triangle {
+        constructor(x, y, length){
+          super(x, y);
+          this.to([x-length/2, y-length/2*Math.sqrt(3)], [x+length/2, y-length/2*Math.sqrt(3)]);
+          //this.color = colors["a"+Math.ceil(Math.random()*4)];
+          this.color = "#DDDDDD";
+          this.direction = new Pt.Vector((Math.random()-0.5) * 6, (Math.random()-0.5) * 6);
+          this.maxAge = Math.random() * 1000;
+          this.age = this.maxAge
+        }
+        animate(time, fps, context){
+          form.fill(this.color);
+          form.triangle(this);
+          this.rotate2D(0.4 / fps, this.orthocenter());
+          this.moveBy(this.direction);
+          this.age -= 0.5;
+          this.color = Pt.Util.toRGBColor("#DDDDDD", true, this.age/this.maxAge);
+          if(this.age < 0 || this.x < -10 || this.x > space.size.x + 10|| this.y < -10 || this.y > space.size.y + 10){
+            space.remove(this);
+          }
         }
       }
+      
+      var lastTime = 0;
+
+      space.add({
+        animate: function(time, fps, context) {
+          if(time - lastTime > 20){
+            space.add(new Particle(space.center.x, space.center.y, 10));
+            //space.add(new ParticleTrack(space.center.x, space.center.y, 10));
+            lastTime = time;
+          }
+        },
+      });
+
+      space.play();
+
+      //var particles = [];
+      for(var i = 0; i < 100; i++){
+        //particles.push(new Particle(space.size.x/2, space.size.y/2, 10));
+        particles.push(new Particle(space.center.x, space.center.y, 10));
+        space.add(particles[i]);
+      }
+
     }
-    
-    var lastTime = 0;
-
-    space.add({
-      animate: function(time, fps, context) {
-        if(time - lastTime > 20){
-          space.add(new Particle(space.center.x, space.center.y, 10));
-          //space.add(new ParticleTrack(space.center.x, space.center.y, 10));
-          lastTime = time;
-        }
-      },
-    });
-
-    space.play();
-
-    //var particles = [];
-    for(var i = 0; i < 100; i++){
-      //particles.push(new Particle(space.size.x/2, space.size.y/2, 10));
-      particles.push(new Particle(space.center.x, space.center.y, 10));
-      space.add(particles[i]);
-    }
-  }*/
+  }
   render(){
     return(
       <div id="pt" className="background-wrapper">
