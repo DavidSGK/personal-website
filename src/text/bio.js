00fe85de128e8f97a2cf93a8b1673a48fddfcc5d
David Kang <hpdk97@gmail.com>
2017-09-13 01:43:32 -0400

    Updated resume, some basic text

--- src/text/bio.js
+++ src/text/bio.js
@@ -1,5 +1,5 @@
 const bio = `
-Hi! I'm currently an undergraduate student at the University of Waterloo in my 1B term of Software Engineering. If I'm not grinding out assignments or looking for something new to learn, you can often find me immersed in creative work - I love digital media production and music, A Cappella being my favorite form. Right now, I'm a jack of all trades, master of none, but I'm always working to change that status.
+Hi! I'm currently an undergraduate student at the University of Waterloo in my 2A term of Software Engineering. If I'm not grinding out assignments or looking for something new to learn, you can often find me immersed in creative work - I love digital media production and music, A Cappella being my favorite form. Right now, I'm a jack of all trades, master of none, but I'm always working to change that status.
 `;
 
 export default bio;
--- src/text/hireme.js
+++ src/text/hireme.js
@@ -1,5 +1,5 @@
 const hireme = `
-I'm interested in co-op/internship opportunities for Summer 2017, so please take a look around and contact me if you're interested!
+I'm interested in co-op/internship opportunities for Winter 2018, so please take a look around and contact me if you're interested!
 `;
 
 export default hireme;
